9793a537cb67d637d64545b8bf525461c6babfe2
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-28 18:22:49 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -10,8 +10,11 @@
 #   end
 # end 
 class Book 
-def initialize(title,author,page_count,genre  )
+def initialize(title,author,page_count,genre)
   @title = title 
+  @author = author 
+  @page_count = page_count
+  @genre = genre
   end
   def title 
   @title = "And Then There Were None"
