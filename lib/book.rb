6bfcfcb6f25ed5989938ccd18db4204f6552e3f9
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-28 18:19:58 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,12 @@
-class Book 
-def initialize(title)
-  @title = title 
-  end
-  def title 
-   @title = "And Then There Were None"
-  end
-  def author 
-    @author = "Agatha Christie"
-  end
-end 
+# class Book 
+# def initialize(title)
+#   @title = title 
+#   end
+#   def title 
+#   @title = "And Then There Were None"
+#   end
+#   def author 
+#     @author = "Agatha Christie"
+#   end
+# end 
 
