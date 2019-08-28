674e025228bbc573c269474fc5bd5fc2dbc36b84
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-28 18:25:05 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,10 +11,10 @@
 # end 
 class Book 
   def initialize(title,author,page_count,genre)
-  @title = title 
-  @author = author 
-  @page_count = page_count
-  @genre = genre
+    @title = title 
+    @author = author 
+    @page_count = page_count
+    @genre = genre
   end
 end
 #   def title 
